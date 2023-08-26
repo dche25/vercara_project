35d7fcf0c8f1397187dcb1329103d48db1ba88fb
Derek Che <chederekacha@gmail.com>
2023-08-26 11:49:00 -0400

    updating project file

--- cicd-tf/cicd.tf
+++ cicd-tf/cicd.tf
@@ -157,7 +157,7 @@ resource "aws_codepipeline" "terraform_pipeline" {
 
       configuration = {
         ConnectionArn = aws_codestarconnections_connection.web-connection.arn
-        FullRepositoryId       = "https://github.com/dche25/vercara_project.git",
+        FullRepositoryId       = "dche25/vercara_project",
         BranchName     = "master",
 
       }
